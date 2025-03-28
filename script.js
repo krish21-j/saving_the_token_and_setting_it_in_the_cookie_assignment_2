18502963f14d033a8ed549b2e5ccd9a3d28dd433
krish21-j <iambakinsquad@gmail.com>
2025-03-28 10:43:10 +0530

    done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const jwt = require("jsonwebtoken");
+
 const encrypt = (payload, secret) => {
-  // your code here and return token
+  return jwt.sign(payload, secret, { expiresIn: "1h" }); 
 };
 
 module.exports = encrypt;
